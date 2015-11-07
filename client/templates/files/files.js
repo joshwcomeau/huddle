5d4329311dd64fd11575ee7afac3349da8dcda2f
Josh Comeau <josh@breather.com>
2015-11-07 13:03:16 -0500

    got file templates working, so you can now view a list of your files. Loading template.

--- client/templates/files/files.js
+++ client/templates/files/files.js
@@ -1,4 +1,12 @@
+Template.files.onCreated(function() {
+  this.autorun( () => this.subscribe('my_files') )
+  
+});
+
 Template.files.helpers({
+  ready: function() {
+    
+  },
   myFiles: function() {
     return Files.find({})
   }
